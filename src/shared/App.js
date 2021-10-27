e6e1299e8cc39b5466307e4c1a994883dcee23c4
dltmdals3785@naver.com <dltmdals3785@naver.com>
2021-10-27 14:30:22 +0900

    Add 이승민 상세페이지 및 게시글 작성페이지

--- src/shared/App.js
+++ src/shared/App.js
@@ -5,13 +5,17 @@ import { history } from "../redux/configureStore";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Text from "../pages/Text";
 import MainPage from "../pages/MainPage";
+import Post from "../pages/Post";
+import PostDetail from "../pages/PostDetail";
 
 function App() {
   return (
     <React.Fragment>
       <ConnectedRouter history={history}>
         <Switch>
-          <Route path='/' exact component={MainPage}></Route>
+          <Route path="/" exact component={MainPage}></Route>
+          <Route path="/post" exact component={Post}></Route>
+          <Route path="/postdetail" exact component={PostDetail}></Route>
         </Switch>
       </ConnectedRouter>
     </React.Fragment>
