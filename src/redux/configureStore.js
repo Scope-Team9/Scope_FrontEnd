9a5eaf13b845db943f513cbe486590864b312db0
kimbumsoo0820 <kbs2082@naver.com>
2021-10-28 02:18:53 +0900

    .

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,12 +3,17 @@ import thunk from "redux-thunk";
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import { connectRouter } from "connected-react-router";
 
+import Post from "./modules/post";
+import Stack from "./modules/stack";
+
 export const history = createBrowserHistory();
 
 const rootReducer = combineReducers({
   //   user: User,
   //   product: Product,
   //   comment : Comment,
+  post: Post,
+  stack: Stack,
 
   // 8. 리덕스에 history를 이제 넣어줄 것이다. 우리가 만든 history와 우리의 라우터가 연결이되는 것이다. 그리고 이것의 우리의 스토어에 저장이되는 것이다.
   router: connectRouter(history),
