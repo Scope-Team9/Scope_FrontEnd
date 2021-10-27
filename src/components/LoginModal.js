b7b798170e83981bbbae71b79e0f36ce87ef12ad
dltmdals3785@naver.com <dltmdals3785@naver.com>
2021-10-27 19:02:25 +0900

    Add 이승민 마이페이지

--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -5,7 +5,7 @@ import { useDispatch } from "react-redux";
 import styled from "styled-components";
 import { userCreators } from "../redux/modules/user";
 import kakao from "../images/임시로고.jpg";
-const LoginModal = props => {
+const LoginModal = (props) => {
   const dispatch = useDispatch();
 
   //모달
@@ -18,43 +18,43 @@ const LoginModal = props => {
     dispatch(userCreators.loginMiddleware());
   };
   return (
-    <Dialog maxWidth={"md"} scroll='paper' open={showModal}>
+    <Dialog maxWidth={"md"} scroll="paper" open={showModal}>
       <ModalWrap>
         <Grid
-          className='모달컨테이너'
-          backgroundColor='#fff'
-          borderRadius='0 0 5px 5px'
-          position='relative'
-          width='100%'
-          height='100%'
+          className="모달컨테이너"
+          backgroundColor="#fff"
+          borderRadius="0 0 5px 5px"
+          position="relative"
+          width="100%"
+          height="100%"
         >
-          <Grid padding='20px 0'>
-            <Grid alignItems='center' position='relative'>
+          <Grid padding="20px 0">
+            <Grid alignItems="center" position="relative">
               <Grid
-                position='absolute'
-                top='-10px'
-                right='20px'
-                color='black'
-                width='20px'
-                padding='10px'
+                position="absolute"
+                top="-10px"
+                right="20px"
+                color="black"
+                width="20px"
+                padding="10px"
               >
-                <Button text='닫기' _onClick={modalClose} />
+                <Button text="닫기" _onClick={modalClose} />
               </Grid>
               <Text
-                padding='0 0 5px 20px'
-                size='40px'
-                bold='800'
-                margin='-10px 0'
-                justifyContent='center'
+                padding="0 0 5px 20px"
+                size="40px"
+                bold="800"
+                margin="-10px 0"
+                justifyContent="center"
               >
                 가입하기
               </Text>
-              <Button margin='1px 0' text='깃허브 로그인' _onClick></Button>
-              <Button margin='1px 0' text='카카오 로그인'></Button>
-              <Button margin='1px 0' text='구글 로그인'></Button>
+              <Button margin="1px 0" text="깃허브 로그인" _onClick></Button>
+              <Button margin="1px 0" text="카카오 로그인"></Button>
+              <Button margin="1px 0" text="구글 로그인"></Button>
             </Grid>
           </Grid>
-          <hr color='#eee' />
+          <hr color="#eee" />
         </Grid>
       </ModalWrap>
     </Dialog>
--- src/shared/App.js
+++ src/shared/App.js
@@ -4,9 +4,11 @@ import { ConnectedRouter } from "connected-react-router";
 import { history } from "../redux/configureStore";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Text from "../pages/Text";
+import Header from "../components/Header";
 import MainPage from "../pages/MainPage";
 import Post from "../pages/Post";
 import PostDetail from "../pages/PostDetail";
+import MyPage from "../pages/MyPage";
 
 function App() {
   return (
@@ -16,6 +18,7 @@ function App() {
           <Route path="/" exact component={MainPage}></Route>
           <Route path="/post" exact component={Post}></Route>
           <Route path="/postdetail" exact component={PostDetail}></Route>
+          <Route path="/mypage" exact component={MyPage}></Route>
         </Switch>
       </ConnectedRouter>
     </React.Fragment>
