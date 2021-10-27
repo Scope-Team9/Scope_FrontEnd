4235f7ce4cedc70772c4e952b99e4f9b678c0d5e
dltmdals3785@naver.com <dltmdals3785@naver.com>
2021-10-27 14:31:14 +0900

    Add 이승민 상세페이지 및 게시글 작성 페이지

---
+++ src/pages/Post.js
@@ -0,0 +1,21 @@
+// Post.js
+
+// import를 한다.
+import React from "react";
+import styled from "styled-components";
+
+import PostList from "../components/PostList";
+import PostWrite from "../components/PostWrite";
+
+// Post의 함수형 컴포넌트를 만든다.
+const Post = (props) => {
+  return (
+    <React.Fragment>
+      <PostList />
+      <PostWrite />
+    </React.Fragment>
+  );
+};
+
+// export를 통해 밖에서도 사용할 수 있도록 설정한다.
+export default Post;
