ab49cdf2af6bd02daa2c91696a675dcff2721c93
kimbumsoo0820 <kbs2082@naver.com>
2021-11-02 11:01:12 +0900

    Add김범수 마크다운 이미지 removeHook addHook

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -5,9 +5,9 @@ import { history } from "../redux/configureStore";
 export const instance = axios.create({
   // baseURL: "http://localhost:3000",
   // baseURL: "http://localhost:4000",
-  baseURL: "http://3.36.94.200",
+  // baseURL: "http://3.36.94.200",
 
-  // baseURL: "http://15.165.159.211",
+  baseURL: "http://15.165.159.211",
 
   headers: {
     "content-type": "application/json; charset=UTF-8",
