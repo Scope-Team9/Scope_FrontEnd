4897cae6dcf938d46807e2a27a4ff9fcbcc7cb8f
kzhxxn <kzhxxn@gmail.com>
2021-11-10 03:10:55 +0900

    카드 디자인 변경

--- src/components/Post.js
+++ src/components/Post.js
@@ -8,13 +8,18 @@ import PostStacks from "./PostStacks";
 import { useSelector, useDispatch } from "react-redux";
 
 import { history } from "../redux/configureStore";
-import { Grid, Image } from "../elements/Index";
+import { Grid, Image, Text } from "../elements/Index";
 
 // Post의 함수형 컴포넌트를 만든다.
 const Post = props => {
   const dispatch = useDispatch();
   const is_mainPage = useSelector(state => state.post.mainpage);
   const [stacks, setStacks] = React.useState();
+  console.log(props.totalMember);
+  console.log(props.recruitmentMember);
+
+  let totalmember = props.totalMember;
+  let recruitmentMember = props.recruitmentMember;
   // console.log("게시자", props.recruitmentMember);
   // console.log("메인포스트아이디", props);
   React.useEffect(() => {
@@ -44,16 +49,35 @@ const Post = props => {
                 );
               })}
             </Grid>
+            <CardHeaderTwo />
           </CardHeader>
+          <Grid
+            borderRadius="50px 30px 30px 30px"
+            bg="#fff"
+            height="260px"
+            position="absolute"
+          ></Grid>
           <DescriptionBox>
+            <ProjectState>{props.projectStatus}</ProjectState>
             <Title>{props.title}</Title>
             <Summary>{props.summary}</Summary>
             <Date>
               {props.startDate}~{props.endDate}
             </Date>
             <Line />
-            <Grid>
-              <ProjectState>{props.projectStatus}</ProjectState>
+            <Grid display="flex" width="100%" justifyContent="space-between">
+              <Grid width="100%">
+                <Grid display="flex">
+                  <ProgressBar>
+                    <HighLight
+                      width={(recruitmentMember / totalmember) * 100 + "%"}
+                    />
+                  </ProgressBar>
+                  <Text margin="0 0 0 10px">
+                    {recruitmentMember + "/" + totalmember}
+                  </Text>
+                </Grid>
+              </Grid>
             </Grid>
           </DescriptionBox>
         </DDescriptionBox>
@@ -72,19 +96,24 @@ const TitleDate = styled.div`
 `;
 
 const DescriptionBox = styled.div`
+  position: relative;
   margin: 30px 20px;
 `;
 const DDescriptionBox = styled.div`
   /* background-color: #fff5f9; */
   border-radius: 30px;
   margin: auto;
+  position: relative;
+  height: 100%;
 `;
 
 const CardHeader = styled.div`
+  z-index: -9;
+  position: relative;
   width: 100%;
   height: 70px;
   background-color: #f1bad1;
-  border-radius: 30px 30px 0px 0px;
+  border-radius: 30px 30px 30px 0px;
   background: rgb(83, 201, 253);
   background: linear-gradient(
     140deg,
@@ -92,6 +121,31 @@ const CardHeader = styled.div`
     rgba(231, 170, 250, 1) 74%,
     rgba(231, 170, 250, 1) 100%
   );
+`;
+
+const CardHeaderTwo = styled.div`
+  z-index: -8;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 70%;
+  height: 160px;
+  background-color: #f1bad1;
+  border-radius: 30px 30px 30px 0px;
+  background: rgb(83, 201, 253);
+  background: linear-gradient(
+    140deg,
+    rgba(83, 201, 253, 1) 0%,
+    rgba(231, 170, 250, 1) 74%,
+    rgba(231, 170, 250, 1) 100%
+  );
+`;
+const ContentBox = styled.div`
+  position: absolute;
+  background-color: #fff;
+  border-radius: 30px 0 30px 0px;
+  width: 100%;
+  z-z-index: 999;
 `;
 
 const Title = styled.h1`
@@ -110,12 +164,9 @@ const Summary = styled.div`
 
   margin-top: 8%;
   overflow: hidden;
+  white-space: nowrap;
+  overflow: hidden;
   text-overflow: ellipsis;
-  display: -webkit-box;
-  line-height: 17px;
-  max-height: 34px;
-  -webkit-line-clamp: 2; /* 표시하고자 하는 라인 수 */
-  -webkit-box-orient: vertical;
 `;
 
 const Date = styled.div`
@@ -139,8 +190,14 @@ const Line = styled.hr`
 `;
 
 const ProjectState = styled.div`
-  margin-left: 80%;
-  margin-bottom: 55px;
+  position: absolute;
+  top: -10px;
+  right: 0px;
+
+  background-color: #eee;
+  margin: auto 0;
+  /* margin-left: 80%;
+  margin-bottom: 55px; */
 
   @media (max-width: 750px) {
     margin-left: 60%;
@@ -151,6 +208,7 @@ const ProjectState = styled.div`
 `;
 
 const ProductImgWrap = styled.div`
+  z-index: -10;
   background-color: white;
   width: 80vw;
   height: 80%;
@@ -184,11 +242,21 @@ const ProductImgWrap = styled.div`
     margin-bottom: 30px;
   }
 `;
-const Ssss = styled.div`
-  height: 200px;
-  width: 200px;
-  background: linear-gradient(rgba(250, 0, 0, 0.5), transparent);
-  background-color: orange; /*this your primary color*/
+
+const ProgressBar = styled.div`
+  border: 3px solid #b29cf4;
+  border-radius: 25px;
+  background: #f1f9ff;
+  width: 100%;
+  height: 15px;
+`;
+
+const HighLight = styled.div`
+  border-radius: 25px;
+  background: #b29cf4;
+  transition: 1s;
+  width: ${props => props.width};
+  height: 15px;
 `;
 
 export default Post;
