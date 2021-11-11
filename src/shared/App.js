b83abeeb27edc81fb19ac0e01451a4b0fb6f7570
kimbumsoo0820 <kbs2082@naver.com>
2021-11-11 15:23:37 +0900

    .

--- src/shared/App.js
+++ src/shared/App.js
@@ -7,7 +7,7 @@ import { useDispatch } from "react-redux";
 import { userCreators } from "../redux/modules/user";
 
 import MainPage from "../pages/MainPage";
-import KakaoRedirect from "./KakaoRedirect";
+import KakaoRedirect from "./kakaoRedirect";
 import GitHubRedirect from "./GitHubRedirect";
 import MyPage from "../pages/MyPage";
 import MyPageInfo from "../components/MyPageInfo";
