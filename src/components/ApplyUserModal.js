d784b702d48b19085035ec5d72d3b96be734c888
kzhxxn <kzhxxn@gmail.com>
2021-11-18 14:26:57 +0900

    fix 유저모달 컴포넌트 분리

--- src/components/ApplyUserModal.js
+++ src/components/ApplyUserModal.js
@@ -7,7 +7,12 @@ import styled from "styled-components";
 import { applyCreators } from "../redux/modules/applyProject";
 import CloseIcon from "@mui/icons-material/Close";
 
-import { Apply, Cancel, TeamExit } from "./applyUserModal/ApplyIndex";
+import {
+  Apply,
+  Cancel,
+  TeamExit,
+  SubmitUrl,
+} from "./applyUserModal/ApplyIndex";
 const ApplyUserModal = props => {
   const dispatch = useDispatch();
   const {
@@ -246,101 +251,7 @@ const ApplyUserModal = props => {
           </ModalWrap>
         )}
         {applyValue === "end" && page === 2 && (
-          <ModalWrap>
-            <Grid height="10%" position="relative">
-              <Grid
-                position="absolute"
-                top="0px"
-                right="10px"
-                width="20px"
-                padding="10px"
-              >
-                <CloseIcon fontSize="large" onClick={modalClose} />
-              </Grid>
-            </Grid>
-            <Grid
-              margin="auto"
-              height="90%"
-              // justifyContent="center"
-              width="90%"
-              alignItems="center"
-              textAlign="center"
-            >
-              <Grid height="10%" textAlign="center">
-                <Text size="30px" bold>
-                  프로젝트주소 소개
-                </Text>
-              </Grid>
-              <Grid height="10%" margin="10px 0">
-                <Text size="14px">
-                  완료된 프로젝트를 소개해주시겠어요? <br />
-                </Text>
-              </Grid>
-              <Grid display="flex" height="40%" width="90%" margin="auto">
-                <Grid
-                  display="flex"
-                  flexDirection="column"
-                  height="80%"
-                  width="30%"
-                  margin="auto"
-                  bg="#eee"
-                >
-                  <Grid
-                    display="flex"
-                    alignItems="center"
-                    justifyContent="center"
-                  >
-                    <Text>프론트엔드</Text>
-                  </Grid>
-                  <Grid
-                    display="flex"
-                    alignItems="center"
-                    justifyContent="center"
-                    bg="#eee"
-                  >
-                    <Text>백엔드</Text>
-                  </Grid>
-                </Grid>
-                <Grid
-                  display="flex"
-                  flexDirection="column"
-                  height="80%"
-                  width="70%"
-                  margin="auto"
-                  bg="#eee"
-                  alignItems="center"
-                >
-                  <Input
-                    height="100%"
-                    _onChange={e => {
-                      console.log(e.target.value);
-                      setFront(e.target.value);
-                    }}
-                  ></Input>
-
-                  <Input
-                    height="100%"
-                    _onChange={e => {
-                      console.log(e.target.value);
-                      setBack(e.target.value);
-                    }}
-                  ></Input>
-                </Grid>
-              </Grid>
-              <Grid display="flex" height="15%" margin="20px auto" width="90%">
-                <Grid>
-                  <Button borderRadius="25px" _onClick={submitUrl}>
-                    제출하기
-                  </Button>
-                </Grid>
-                <Grid>
-                  <Button borderRadius="25px" _onClick={() => {}}>
-                    다음에제출
-                  </Button>
-                </Grid>
-              </Grid>
-            </Grid>
-          </ModalWrap>
+          <SubmitUrl modalClose={modalClose} postId={postId} />
         )}
         {applyValue === "starterLiked" && (
           <ModalWrap>
@@ -493,101 +404,7 @@ const ApplyUserModal = props => {
           </ModalWrap>
         )}
         {applyValue === "submit" && passdedMenber[0].userId === isMe && (
-          <ModalWrap>
-            <Grid height="10%" position="relative">
-              <Grid
-                position="absolute"
-                top="0px"
-                right="10px"
-                width="20px"
-                padding="10px"
-              >
-                <CloseIcon fontSize="large" onClick={modalClose} />
-              </Grid>
-            </Grid>
-            <Grid
-              margin="auto"
-              height="90%"
-              // justifyContent="center"
-              width="90%"
-              alignItems="center"
-              textAlign="center"
-            >
-              <Grid height="10%" textAlign="center">
-                <Text size="30px" bold>
-                  프로젝트주소 소개
-                </Text>
-              </Grid>
-              <Grid height="10%" margin="10px 0">
-                <Text size="14px">
-                  완료된 프로젝트를 소개해주시겠어요? <br />
-                </Text>
-              </Grid>
-              <Grid display="flex" height="40%" width="90%" margin="auto">
-                <Grid
-                  display="flex"
-                  flexDirection="column"
-                  height="80%"
-                  width="30%"
-                  margin="auto"
-                  bg="#eee"
-                >
-                  <Grid
-                    display="flex"
-                    alignItems="center"
-                    justifyContent="center"
-                  >
-                    <Text>프론트엔드</Text>
-                  </Grid>
-                  <Grid
-                    display="flex"
-                    alignItems="center"
-                    justifyContent="center"
-                    bg="#eee"
-                  >
-                    <Text>백엔드</Text>
-                  </Grid>
-                </Grid>
-                <Grid
-                  display="flex"
-                  flexDirection="column"
-                  height="80%"
-                  width="70%"
-                  margin="auto"
-                  bg="#eee"
-                  alignItems="center"
-                >
-                  <Input
-                    height="100%"
-                    _onChange={e => {
-                      console.log(e.target.value);
-                      setFront(e.target.value);
-                    }}
-                  ></Input>
-
-                  <Input
-                    height="100%"
-                    _onChange={e => {
-                      console.log(e.target.value);
-                      setBack(e.target.value);
-                    }}
-                  ></Input>
-                </Grid>
-              </Grid>
-              <Grid display="flex" height="15%" margin="20px auto" width="90%">
-                <Grid>
-                  <Button borderRadius="25px" _onClick={submitUrl}>
-                    제출하기
-                  </Button>
-                </Grid>
-                <Grid>
-                  <Button borderRadius="25px" _onClick={() => {}}>
-                    다음에제출
-                  </Button>
-                </Grid>
-              </Grid>
-            </Grid>
-          </ModalWrap>
+          <SubmitUrl modalClose={modalClose} postId={postId} />
         )}
         {applyValue === "memberLiked" && (
           <ModalWrap>
