8ddf583c87e1ce3fed5ae025aca055a3dad9ff00
kzhxxn <kzhxxn@gmail.com>
2021-11-18 14:20:04 +0900

    fix url전송모달 분리

--- src/components/applyUserModal/ApplyIndex.js
+++ src/components/applyUserModal/ApplyIndex.js
@@ -1,5 +1,6 @@
 import Apply from "./Apply";
 import Cancel from "./Cancel";
 import TeamExit from "./TeamExit";
+import SubmitUrl from "./SubmitUrl";
 
-export { Apply, Cancel, TeamExit };
+export { Apply, Cancel, TeamExit, SubmitUrl };
