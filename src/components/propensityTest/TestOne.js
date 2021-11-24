8a5d81f30118fbb714b239f76bb1e0b024fa72d4
kzhxxn <kzhxxn@gmail.com>
2021-11-24 14:06:36 +0900

    병합

--- src/components/propensityTest/TestOne.js
+++ src/components/propensityTest/TestOne.js
@@ -1,59 +1,54 @@
 import React from "react";
-import styled from "styled-components";
+import TestData from "./Testdata.json";
 import { Grid, Button, Text } from "../../elements/Index";
 
 const TestOne = props => {
   const { handleUserCreate, handleMemberCreate } = props;
-  const [nowClickU, setNowClickU] = React.useState(null);
-  const [prevClickU, setPrevClickU] = React.useState(null);
-  const [nowClickMB, setNowClickMB] = React.useState(null);
-  const [prevClickMB, setPrevClickMB] = React.useState(null);
-
-  const clickUser = answer => {
-    setNowClickU(answer);
-  };
-  const clickMember = answer => {
-    setNowClickMB(answer);
-  };
-
-  ///유저 설문 버튼 클릭유지
-  React.useEffect(
-    e => {
-      //값이 들어오면 해당 버튼 css 변경
-      if (nowClickU !== null) {
-        let current = document.getElementById(nowClickU);
-        current.style.backgroundColor = "#554475";
-        current.style.color = "#fff";
-      }
-      //다른 버튼이 클릭될경우 기존 스테이트값이 이전버튼스테이트로 이동
-      if (prevClickU !== null) {
-        let prev = document.getElementById(prevClickU);
-        prev.style.color = "#554475";
-        prev.style.backgroundColor = "#fff";
-      }
-      setPrevClickU(nowClickU);
-    },
-    [nowClickU]
+  const [nowClickU, setNowClickU] = React.useState(
+    TestData.userbtn.filter(btn => btn.question === "Q1")
+  );
+  const [nowClickMB, setNowClickMB] = React.useState(
+    TestData.memberbtn.filter(btn => btn.question === "Q1")
   );
 
-  //멤버 설문 버튼 클릭유지
-  React.useEffect(
-    e => {
-      if (nowClickMB !== null) {
-        let current = document.getElementById(nowClickMB);
-        current.style.backgroundColor = "#554475";
-        current.style.color = "#fff";
-      }
+  const clickUser = btnUserId => {
+    console.log(btnUserId);
+    setNowClickU(state => {
+      return state.map(stateItem => {
+        if (stateItem.id === btnUserId) {
+          return { ...stateItem, active: !stateItem.active };
+        }
+        return stateItem;
+      });
+    });
+    setNowClickU(state => {
+      return state.map(stateItem => {
+        if (stateItem.id !== btnUserId && stateItem.active === true) {
+          return { ...stateItem, active: !stateItem.active };
+        }
+        return stateItem;
+      });
+    });
+  };
 
-      if (prevClickMB !== null) {
-        let prev = document.getElementById(prevClickMB);
-        prev.style.color = "#554475";
-        prev.style.backgroundColor = "#fff";
-      }
-      setPrevClickMB(nowClickMB);
-    },
-    [nowClickMB]
-  );
+  const clickMember = btnMemberId => {
+    setNowClickMB(state => {
+      return state.map((stateItem, idx) => {
+        if (stateItem.id === btnMemberId) {
+          return { ...stateItem, active: !stateItem.active };
+        }
+        return stateItem;
+      });
+    });
+    setNowClickMB(state => {
+      return state.map(stateItem => {
+        if (stateItem.id !== btnMemberId && stateItem.active === true) {
+          return { ...stateItem, active: !stateItem.active };
+        }
+        return stateItem;
+      });
+    });
+  };
 
   return (
     <Grid>
@@ -63,56 +58,42 @@ const TestOne = props => {
             <Grid margin="5px 0">
               Q1. <b>팀 회의할 때 당신의 모습</b>에 더 가까운 것은?
             </Grid>
-            <Button
-              isId="UL"
-              isValue="L"
-              isTest
-              _onClick={e => {
-                clickUser(e.target.id);
-                handleUserCreate(e.target.value);
-              }}
-            >
-              내 주장을 펼치며 회의 분위기를 탄탄하게 이끌기
-            </Button>
-            <Button
-              isTest
-              isId="UF"
-              isValue="F"
-              _onClick={e => {
-                clickUser(e.target.id);
-                handleUserCreate(e.target.value);
-              }}
-            >
-              다른 사람들의 의견을 들으며 뭘 맡아서 해야할지 파악하기
-            </Button>
+            {nowClickU.map((btn, idx) => (
+              <Grid key={btn.id} {...btn}>
+                <Button
+                  isId={btn.id}
+                  isValue={btn.value}
+                  isTest
+                  text={btn.text}
+                  isActive={btn.active}
+                  _onClick={e => {
+                    clickUser(e.target.id);
+                    handleUserCreate(e.target.value);
+                  }}
+                ></Button>
+              </Grid>
+            ))}
           </Grid>
         </Grid>
         <Grid>
           <Grid margin="5px 0">
-            Q2. <b>팀 회의할 때 선호하는 팀원의 모습</b>에 더 가까운 것은?
+            Q1. <b>팀 회의할 때 선호하는 팀원의 모습</b>에 더 가까운 것은?
           </Grid>
-          <Button
-            isTest
-            isId="ML"
-            isValue="L"
-            _onClick={e => {
-              clickMember(e.target.id);
-              handleMemberCreate(e.target.value);
-            }}
-          >
-            내 주장을 펼치며 회의 분위기를 탄탄하게 이끌기
-          </Button>
-          <Button
-            isTest
-            isId="MF"
-            isValue="F"
-            _onClick={e => {
-              clickMember(e.target.id);
-              handleMemberCreate(e.target.value);
-            }}
-          >
-            다른 사람들의 의견을 들으며 뭘 맡아서 해야할지 파악하기
-          </Button>
+          {nowClickMB.map((btn, idx) => (
+            <Grid key={btn.id} {...btn}>
+              <Button
+                isId={btn.id}
+                isValue={btn.value}
+                isTest
+                text={btn.text}
+                isActive={btn.active}
+                _onClick={e => {
+                  clickMember(e.target.id);
+                  handleMemberCreate(e.target.value);
+                }}
+              ></Button>
+            </Grid>
+          ))}
         </Grid>
       </Grid>
     </Grid>
