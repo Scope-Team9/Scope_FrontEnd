1a4ce83949e81cd2b142c5384fd5e3df17d37fe8
dltmdals3785@naver.com <dltmdals3785@naver.com>
2021-11-25 17:22:37 +0900

    Add 이승민 알림

--- src/components/headerFunction/Bell.js
+++ src/components/headerFunction/Bell.js
@@ -1,6 +1,32 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import StompJs from "@stomp/stompjs";
+import SockJS from "sockjs-client";
+import { actionCreators } from "../../redux/modules/user";
 
 const Bell = (props) => {
+  const dispatch = useDispatch();
+  const userId = useSelector((state) => state.user.user.userId);
+  console.log("유저 아이디", userId);
+  const token = document.cookie.includes("USER_TOKEN");
+
+  const sock = new SockJS(``);
+  const ws = StompJs.over(sock);
+  ws.connect({}, () => {
+    if (!token) {
+      return null;
+    }
+    console.log("소리질러");
+    ws.subscribe(
+      `/sub/${userId}`,
+      async (msg) => {
+        const alarmDate = JSON.parse(msg.body);
+        await dispatch(actionCreators.readAlarm(false));
+        dispatch(actionCreators.updateAlarm(alarmDate));
+      },
+      { token }
+    );
+  });
   return (
     <React.Fragment>
       <div></div>
