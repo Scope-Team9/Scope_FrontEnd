c395e32a6f5a9e921c968bac680131b728d9e170
kzhxxn <kzhxxn@gmail.com>
2021-11-30 17:59:00 +0900

    fix 북마크 수정

--- src/components/postDetail/rightContents/BookMark.js
+++ src/components/postDetail/rightContents/BookMark.js
@@ -9,23 +9,23 @@ import BookmarkIcon from "@mui/icons-material/Bookmark";
 import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 
 // Bookmark의 함수형 컴포넌트를 만든다.
-const Bookmark = (props) => {
+const Bookmark = props => {
   return (
     <React.Fragment>
       <BookMarkMedia>
         {props.userId !== props.postUserId && (
           <Grid
-            width="50px"
+            width="10px"
             position="absolute"
             top="0px"
-            right="10px"
-            height="50px"
+            right="50px"
+            height="10px"
           >
             <Grid _onClick={props.ToggleBookMark} cursor="pointer">
               {!props.passedData?.bookmarkChecked ? (
-                <BookmarkBorderIcon sx={{ color: "#17334A", fontSize: 60 }} />
+                <BookmarkBorderIcon sx={{ color: "#17334A", fontSize: 50 }} />
               ) : (
-                <BookmarkIcon sx={{ color: "#17334A", fontSize: 60 }} />
+                <BookmarkIcon sx={{ color: "#17334A", fontSize: 50 }} />
               )}
             </Grid>
           </Grid>
