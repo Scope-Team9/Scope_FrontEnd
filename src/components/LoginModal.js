6367748726bca19bd5a5a83e7d1895c292e2a3bf
kimbumsoo0820 <kbs2082@naver.com>
2021-12-02 12:58:30 +0900

    받기 전

--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -10,10 +10,12 @@ import Select from "react-select";
 import PropensityTest from "./propensityTest/PropensityTest";
 import CloseIcon from "@mui/icons-material/Close";
 
-const LoginModal = props => {
+const LoginModal = (props) => {
   const dispatch = useDispatch();
-  const userInfo = useSelector(state => state.user);
-  const sigunupModalState = useSelector(state => state.user.sigunupModalState);
+  const userInfo = useSelector((state) => state.user);
+  const sigunupModalState = useSelector(
+    (state) => state.user.sigunupModalState
+  );
 
   var regExpNick = /^[a-zA-Z0-9ㄱ-ㅎ|ㅏ-ㅣ|가-힣]{2,5}$/;
   var regExpEmail =
@@ -61,7 +63,7 @@ const LoginModal = props => {
   // console.log("sns아이디", userInfo.snsId);
 
   //닉네임 체크 미들웨어
-  const nickCheck = nickName => {
+  const nickCheck = (nickName) => {
     if (nickName === undefined) {
       alert("닉네임을 입력 해주세요.");
       return false;
@@ -75,7 +77,7 @@ const LoginModal = props => {
   };
 
   //이메일 체크 미들웨어
-  const emailCheck = email => {
+  const emailCheck = (email) => {
     if (nickName === "") {
       alert("이메일을 입력 해주세요.");
       return false;
@@ -114,7 +116,7 @@ const LoginModal = props => {
   };
 
   const customStyles = {
-    control: styles => ({
+    control: (styles) => ({
       ...styles,
       backgroundColor: "white",
       borderRadius: "20px",
@@ -277,7 +279,7 @@ const LoginModal = props => {
                         padding="0 0 0 23px"
                         height="100%"
                         placeholder="닉네임을 입력해주세요"
-                        _onChange={e => {
+                        _onChange={(e) => {
                           setNickName(e.target.value);
                         }}
                       >
@@ -316,7 +318,7 @@ const LoginModal = props => {
                         options={techStackOption}
                         className="basic-multi-select"
                         classNamePrefix="select"
-                        onChange={e => {
+                        onChange={(e) => {
                           let techStack = [];
                           let arr = e;
                           let idx = 0;
@@ -450,11 +452,11 @@ const LoginModal = props => {
                       // "https://github.com/login/oauth/authorize?client_id=5bb2c0fab941fb5b8f9f&scope=repo:status read:repo_hook user:email&redirect_uri=http://kbumsoo.s3-website.ap-northeast-2.amazonaws.com/user/github/callback";
 
                       //local
-                      "https://github.com/login/oauth/authorize?client_id=5bb2c0fab941fb5b8f9f&scope=repo:status read:repo_hook user:email&redirect_uri=http://localhost:3000/user/github/callback";
+                      // "https://github.com/login/oauth/authorize?client_id=5bb2c0fab941fb5b8f9f&scope=repo:status read:repo_hook user:email&redirect_uri=http://localhost:3000/user/github/callback";
 
-                    // 최종 주소
+                      // 최종 주소
 
-                    // "https://github.com/login/oauth/authorize?client_id=5bb2c0fab941fb5b8f9f&scope=repo:status read:repo_hook user:email&redirect_uri=https://scopewith.com/user/github/callback";
+                      "https://github.com/login/oauth/authorize?client_id=5bb2c0fab941fb5b8f9f&scope=repo:status read:repo_hook user:email&redirect_uri=https://scopewith.com/user/github/callback";
                   }}
                 >
                   깃허브로그인
