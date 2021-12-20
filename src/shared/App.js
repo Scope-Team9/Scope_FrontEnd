f785bc40d87ebe268d2df2aa0692b596f2254fd5
kzhxxn <kzhxxn@gmail.com>
2021-12-21 01:06:10 +0900

    Add 강지훈 성능최적화를 위한 번들사이즈 측정 및 이미지 리사이즈

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,32 +1,30 @@
 /* eslint-disable */
-import { Grid } from "../elements/Index";
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import "./App.css";
 import { ConnectedRouter } from "connected-react-router";
 import { history } from "../redux/configureStore";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { userCreators } from "../redux/modules/user";
 
-import MainPage from "../pages/MainPage";
-import KakaoRedirect from "./kakaoRedirect";
-import GitHubRedirect from "./GitHubRedirect";
+const MainPage = lazy(() => import("../pages/MainPage"));
+const KakaoRedirect = lazy(() => import("./kakaoRedirect"));
+const GitHubRedirect = lazy(() => import("./GitHubRedirect"));
 
-import MyPageInfo from "../components/MyPageInfo";
-import PostWrite from "../pages/PostWrite";
-import PostEdit from "../pages/PostEdit";
-import PostDetail from "../pages/PostDetail";
-import Markdown from "../components/Markdown";
-import MarkdownRead from "../components/MarkdownRead";
-import Header from "../components/Header";
-import Message from "../components/headerFunction/Message";
-import Footer from "../components/Footer";
-import NotFound from "../shared/NotFound";
-import { ContactSupportOutlined } from "@material-ui/icons";
+const MyPageInfo = lazy(() => import("../components/MyPageInfo"));
+const PostWrite = lazy(() => import("../pages/PostWrite"));
+const PostEdit = lazy(() => import("../pages/PostEdit"));
+const PostDetail = lazy(() => import("../pages/PostDetail"));
+const Markdown = lazy(() => import("../components/Markdown"));
+
+const Header = lazy(() => import("../components/Header"));
+const Message = lazy(() => import("../components/headerFunction/Message"));
+const Footer = lazy(() => import("../components/Footer"));
+const NotFound = lazy(() => import("../shared/NotFound"));
 
 function App() {
-  const isLogin = useSelector((state) => state.user.isLogin);
-  const userInfo = useSelector((state) => state.user);
+  const isLogin = useSelector(state => state.user.isLogin);
+  const userInfo = useSelector(state => state.user);
   const isCookie = document.cookie.split("=")[1];
   const dispatch = useDispatch();
 
@@ -37,34 +35,40 @@ function App() {
   }, [isLogin, isCookie]);
 
   return (
-    <React.Fragment>
-      <div className="App">
-        <Header />
-        <ConnectedRouter history={history}>
-          <Switch>
-            <Route path="/" exact component={MainPage}></Route>
-            {/* <Route path="/message" exact component={Message}></Route> */}
-            <Route path="/mypage/:id" exact component={MyPageInfo}></Route>
-            <Route path="/postadd" exact component={PostWrite}></Route>
-            <Route path="/postedit/:id" exact component={PostEdit}></Route>
-            <Route path="/addmarkdown" exact component={Markdown}></Route>
-            <Route path="/postdetail/:id" exact component={PostDetail}></Route>
-            <Route
-              path="/user/kakao/callback"
-              exact
-              component={KakaoRedirect}
-            ></Route>
-            <Route
-              path="/user/github/callback"
-              exact
-              component={GitHubRedirect}
-            ></Route>
-            <Route component={NotFound}></Route>
-          </Switch>
-        </ConnectedRouter>
-        <Footer userInfo={userInfo} />
-      </div>
-    </React.Fragment>
+    <Router>
+      <Suspense fallback={<></>}>
+        <div className="App">
+          <Header />
+          <ConnectedRouter history={history}>
+            <Switch>
+              <Route path="/" exact component={MainPage}></Route>
+              {/* <Route path="/message" exact component={Message}></Route> */}
+              <Route path="/mypage/:id" exact component={MyPageInfo}></Route>
+              <Route path="/postadd" exact component={PostWrite}></Route>
+              <Route path="/postedit/:id" exact component={PostEdit}></Route>
+              <Route path="/addmarkdown" exact component={Markdown}></Route>
+              <Route
+                path="/postdetail/:id"
+                exact
+                component={PostDetail}
+              ></Route>
+              <Route
+                path="/user/kakao/callback"
+                exact
+                component={KakaoRedirect}
+              ></Route>
+              <Route
+                path="/user/github/callback"
+                exact
+                component={GitHubRedirect}
+              ></Route>
+              <Route component={NotFound}></Route>
+            </Switch>
+          </ConnectedRouter>
+          <Footer userInfo={userInfo} />
+        </div>
+      </Suspense>
+    </Router>
   );
 }
 
