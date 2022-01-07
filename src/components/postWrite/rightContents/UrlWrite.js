cc7f4e396cb25cc058e4bb7286e4be0efc6ee405
dltmdals3785@naver.com <dltmdals3785@naver.com>
2022-01-07 17:02:00 +0900

    (Refactor) 이승민 게시글 작성페이지

--- src/components/postWrite/rightContents/UrlWrite.js
+++ src/components/postWrite/rightContents/UrlWrite.js
@@ -6,7 +6,7 @@ import React from "react";
 import styled from "styled-components";
 import { Grid, Input, Text } from "../../../elements/Index";
 
-// TitleWrite의 함수형 컴포넌트를 만든다..
+// TitleWrite의 함수형 컴포넌트를 만든다.
 const UrlWrite = (props) => {
   return (
     <React.Fragment>
